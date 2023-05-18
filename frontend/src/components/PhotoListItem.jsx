b9bf4785686aa6792671a499d433f521f9b591d7
David Luc <luc.ky@me.com>
2023-05-18 16:29:49 -0400

    Update to display photo

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,17 +1,25 @@
+import React from "react";
 
-import React from 'react';
+import "../styles/PhotoListItem.scss";
 
-import '../styles/PhotoListItem.scss';
-
-const PhotoListItem = () => {
-  /* Insert React */
-}
+const PhotoListItem = (props) => {
+  const { id, username, imageSource, hideUserName } = props;
+  return (
+    <article className="photo-list--item">
+      <img
+        src={imageSource}
+        alt={`${username}'s photo`}
+        className="photo-list--image"
+      />
+    </article>
+  );
+};
 
 PhotoListItem.defaultProps = {
-  username: 'Jacob',
+  username: "Jacob",
   imageSource: `${process.env.PUBLIC_URL}/Image.jpg`,
   id: 1,
   hideUserName: false,
-}
+};
 
-export default PhotoListItem
+export default PhotoListItem;
