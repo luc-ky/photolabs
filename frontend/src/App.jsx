b708978ae43120eba8a432f699b3ab036e3907ea
David Luc <luc.ky@me.com>
2023-05-19 00:20:44 -0400

    Add HomeRoute component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 
+import HomeRoute from "./components/HomeRoute";
 import "./App.scss";
-import PhotoList from "./components/PhotoList";
-import TopNavigationBar from './components/TopNavigationBar';
 
 const App = () => {
   return (
     <div className="App">
-      <TopNavigationBar />
-      <PhotoList />
+      <HomeRoute />
     </div>
   );
 };
---
+++ frontend/src/components/HomeRoute.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import TopNavigationBar from "../components/TopNavigationBar";
+import PhotoList from "../components/PhotoList";
+
+import "../styles/HomeRoute.scss";
+
+const HomeRoute = () => {
+  return (
+    <div className="home-route">
+      <TopNavigationBar />
+      <PhotoList />
+    </div>
+  );
+};
+
+export default HomeRoute;
